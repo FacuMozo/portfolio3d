f965654fd7be4ae5622c9beba36b9f4f9d3e2fee
FacuMozo <facumozo98@gmail.com>
2024-04-17 12:42:57 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import reactRefresh from '@vitejs/plugin-react-refresh';
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
